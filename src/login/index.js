56ac69f616b9fc5e96cf8c9dc504ad9feef87366
sanarefixd <sahana@refixd.com>
2023-01-18 23:20:54 +0530

    adding login

--- src/login/index.js
+++ src/login/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-function login() {
-  return <div>login</div>;
-}
+const Login = () => {
+  return (
+    <div>
+      <h1>LOGIN</h1>
+      <div>
+        <label>
+          Username:
+          <input type="text" name="username" placeholder="username" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password:
+          <input type="password" name="password" placeholder="password" />
+        </label>
+      </div>
+      <div>
+        <button>LOGIN</button>
+        <a href="">Forgot Password</a>
+      </div>
+    </div>
+  );
+};
 
-export default login;
+export default Login;
