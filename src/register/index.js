bc859e92c0980e5562a1ad5c7ccb004aa457361a
Engamba <engambasingha@gmail.com>
2023-01-19 11:35:15 +0530

    Adding styles

--- src/register/index.js
+++ src/register/index.js
@@ -1,7 +1,47 @@
 import React from "react";
+import {
+  StyledButton,
+  StyledDiv,
+  StyledForm,
+  StyledHeading,
+  StyledInput,
+} from "./styled";
 
-function Register() {
-  return <div>Register</div>;
-}
+const Register = () => {
+  return (
+    <StyledDiv>
+      <StyledHeading>Register</StyledHeading>
+      <div>
+        <StyledForm onSubmit="">
+          <StyledInput>
+            Username
+            <input type="text" name="username" placeholder="username" />
+          </StyledInput>
+
+          <StyledInput>
+            Email
+            <input type="text" name="email" placeholder="email" />
+          </StyledInput>
+
+          <StyledInput>
+            Password
+            <input type="password" name="password" placeholder="password" />
+          </StyledInput>
+
+          <StyledInput>
+            Confirm Password
+            <input
+              type="password"
+              name="password"
+              placeholder="re-enter password"
+            />
+          </StyledInput>
+
+          <StyledButton onClick={""}>Register</StyledButton>
+        </StyledForm>
+      </div>
+    </StyledDiv>
+  );
+};
 
 export default Register;
---
+++ src/register/styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.label`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+`;
+export const StyledForm = styled.form`
+  /* border: 1px solid red; */
+`;
+export const StyledDiv = styled.div`
+  border: 1px solid;
+  width: 500px;
+`;
+export const StyledHeading = styled.h1`
+  font-size: 25px;
+  padding: 15px;
+`;
+export const StyledButton = styled.button`
+  background-color: blue;
+  border: none;
+  margin: 15px;
+  padding: 10px;
+  color: white;
+`;
