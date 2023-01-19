f66373e5d6c08b7945577446d551dcd8b8d66da3
Engamba <engambasingha@gmail.com>
2023-01-19 16:09:35 +0530

    Adding changes

--- src/forgetpassword/index.js
+++ src/forgetpassword/index.js
@@ -1,21 +1,19 @@
 import React from "react";
 
 function Forgetpassword() {
-  
-
-  return 
-  <div className="container">
-    <h1>Forget Password</h1>
-    <div className="fpassword">
-      <label name="password">New Password</label>
-    <input type="text" placeholder="enter your password"></input>
+  return (
+    <div className="container">
+      <h1>Forget Password</h1>
+      <div className="fpassword">
+        <label name="password">New Password</label>
+        <input type="text" placeholder="enter your password"></input>
+      </div>
+      <div className="rpassword">
+        <label name="reset">confirm Password</label>
+        <input type="text" placeholder="enter reset password"></input>
+      </div>
     </div>
-    <div className="rpassword">
-      <label name="reset">confirm Password</label>
-      <input type="text" placeholder="enter reset password"></input>
-    </div>
-    
-  </div>;
+  );
 }
 
 export default Forgetpassword;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Forgetpassword from './forgetpassword';
-=======
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-import Register from "./register";
-// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Forgetpassword from "./forgetpassword";
+import "./index.css";
+import Register from "./register";
 import Login from "./login";
-<<<<<<< HEAD
-=======
->>>>>>> 56ac69f616b9fc5e96cf8c9dc504ad9feef87366
->>>>>>> 8699e8668131c06bed289623249c1eb474653575
->>>>>>> a4b26d20bac8524a30e92b720e84a974ba9ec789
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< HEAD
- <div>
-  <
-  <Forgetpassword/>
- </div>
-=======
   <div>
     <Register />
     <Login />
+    <Forgetpassword />
   </div>
->>>>>>> 8699e8668131c06bed289623249c1eb474653575
 );
 
 // If you want to start measuring performance in your app, pass a function
