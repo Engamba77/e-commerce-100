a671baae07fe494218062610307b6e4ea1a906c8
sanarefixd <sahana@refixd.com>
2023-01-19 23:38:15 +0530

    adding product details and listing

---
+++ src/products/prodetails.js
@@ -0,0 +1,53 @@
+/* import React from "react";
+import ProList from "./prolist";
+
+const Prodetails = () => {
+  const products = [
+    {
+      Price: "$9.99",
+      img: "https://cdn.shopify.com/s/files/1/0579/7924/0580/t/18/assets/Alphonso-mango.jpg?v=1661863920",
+      Title: "Mango",
+      Description: "Description of Mango",
+    },
+    {
+      Price: "$9.99",
+      img: "https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/10/apple-basket-apples-bushel-1296x728-header.jpg?w=1155&h=1528",
+      Title: "Apple",
+      Description: "Description of Apple",
+    },
+    {
+      Price: "$9.99",
+      img: "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQsVTPslWEK2BcdeFFXxawmkKrqnJeZgum9p9kn1hYiqidKZtdV2P5FTF54GMQp-MtCchhoVzKf-2Ta8lI",
+      Title: "Blueberry",
+      Description: "Description of Blueberry",
+    },
+    {
+      Price: "$9.99",
+      img: "https://www.healthifyme.com/blog/wp-content/uploads/2022/01/Jackfruit-1.jpg",
+      Title: "Jackfruit",
+      Description: "Description of Jackfruit",
+    },
+    {
+      Price: "$9.99",
+      img: "https://www.bangaloreagrico.in/wp-content/uploads/2018/11/ORANGE.jpg",
+      Title: "Orange",
+      Description: "Description of Orange",
+    },
+  ];
+
+  return (
+    <div>
+      {products.map((product) => {
+        return (
+          <div>
+            <ProList product={product} />
+        </div>
+        )
+        
+      })}
+    </div>
+  );
+};
+
+export default Prodetails;
+ */
---
+++ src/products/prolist.js
@@ -0,0 +1,18 @@
+/* import React from "react";
+
+function ProList({ product }) {
+  return (
+    <div>
+      <img src={product.img} alt="" />
+      <div>
+        <p>{product.Title}</p>
+        <p>{product.Description}</p>
+        <p>{product.Price}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProList;
+
+ */
