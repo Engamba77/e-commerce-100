92746ea02c07c4eb23efa4230ae95ff19f3d5fef
Ajoysingh24 <ajoysingh2403@gmail.com>
2023-01-20 11:17:50 +0530

    adding pass style

--- src/forgetpassword/index.js
+++ src/forgetpassword/index.js
@@ -1,17 +1,19 @@
 import React from "react";
+import "./forget.css"
 
 function Forgetpassword() {
   return (
     <div className="container">
-      <h1>Forget Password</h1>
+      <h1 id="head">Forget Password</h1>
       <div className="fpassword">
-        <label name="password">New Password</label>
-        <input type="text" placeholder="enter your password"></input>
-      </div>
+        <label name="password" className="labelname">New Password</label> <br/>
+        <input type="text" placeholder="enter new password" className="inputname"></input>
+      </div> <br/>
       <div className="rpassword">
-        <label name="reset">confirm Password</label>
-        <input type="text" placeholder="enter reset password"></input>
+        <label name="reset" className="labelname">confirm Password</label> <br/>
+        <input type="text" placeholder="enter confirm password" className="inputname"></input>
       </div>
+      <div className="buttonok"><button id="buton">Submit</button></div>
     </div>
   );
 }
