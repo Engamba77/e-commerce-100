af69f32e1e6768a182512ecce0fb18d39793f02c
Engamba <engambasingha@gmail.com>
2023-01-22 22:29:47 +0530

    New Changes index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-<<<<<<< HEAD
-=======
-import reportWebVitals from "./reportWebVitals";
-import "./index.css";
-
->>>>>>> 489ab66c5757c2f1f447cd125c5adda8d17224b9
 import Pages from "./pages";
-
-import Register from "./register";
-import Login from "./login";
-import Forgetpassword from "./forgetpassword";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Pages />);
