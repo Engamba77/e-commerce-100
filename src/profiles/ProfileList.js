0b5582d8153c4d9c0de67a723ba9a952ff643c06
sanarefixd <sahana@refixd.com>
2023-01-23 17:00:18 +0530

    adding profiles

---
+++ src/profiles/ProfileList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ProfileList(item) {
+  return (
+    <div>
+      <img src={item.images} alt="" />
+      <p>{item.name}</p>
+      <p>{item.email}</p>
+    </div>
+  )
+}
+
+export default ProfileList
