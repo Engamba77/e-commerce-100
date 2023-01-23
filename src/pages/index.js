a9802b3bb9cfecf1782f66fea78597fe889d97ed
Engamba <engambasingha@gmail.com>
2023-01-23 17:10:45 +0530

    adding changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProfileList from "../api";
 import Forgetpassword from "../forgetpassword";
 import Login from "../login";
 import Register from "../register";
-import Layout from "./Layout";
+import Home from "./Home";
 import NotFound from "./NotFound";
 
 const Pages = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/forgot password" element={<Forgetpassword />}></Route>
         <Route path="*" element={<NotFound />}></Route>
+        <Route path="/profile list" element={<ProfileList />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/products/prolist.js
+++ src/products/prolist.js
@@ -1,18 +1,16 @@
-/* import React from "react";
+// import React from "react";
 
-function ProList({ product }) {
-  return (
-    <div>
-      <img src={product.img} alt="" />
-      <div>
-        <p>{product.Title}</p>
-        <p>{product.Description}</p>
-        <p>{product.Price}</p>
-      </div>
-    </div>
-  );
-}
+// function ProList({ product }) {
+//   return (
+//     <div>
+//       <img src={product.img} alt="" />
+//       <div>
+//         <p>{product.Title}</p>
+//         <p>{product.Description}</p>
+//         <p>{product.Price}</p>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default ProList;
-
- */
+// export default ProList;
